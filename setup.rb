46a9ce273625a4b76e0c533b99c1c87d8b10cf9c
Yilin Zhang <yzhang0129@outlook.com>
2019-07-14 21:39:09 +0800

    Replace fish setup script with ruby script.

---
+++ setup.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/ruby
+
+class Linker
+  def link(relative_path, link_dir)
+    link_file(get_file_path(relative_path), link_dir)
+  end
+
+  private
+
+  def link_file(file_path, link_dir)
+    file_name = File.basename(file_path)
+    link_path = File.join(link_dir, file_name)
+    if File.exist?(link_path)
+      File.rename(link_path, link_path+".bak")
+    end
+    File.symlink(file_path, link_path)
+  end
+
+  def get_file_path(relative_path)
+    current_dir = File.expand_path(File.dirname(__FILE__))
+    File.join(current_dir, relative_path)
+  end
+end
+
+HOME = Dir.home
+CONFIG = File.join(HOME, ".config")
+
+PROFILE = File.join(HOME, ".profile")
+if File.exist?(PROFILE)
+  File.open(PROFILE, "a+") do |file|
+    file.puts("source ~/.myprofile")
+  end
+end
+
+linker = Linker.new
+linker.link(".Xmodmap", HOME)
+linker.link(".xprofile", HOME)
+linker.link(".myprofile", HOME)
+linker.link(".bashrc", HOME)
+linker.link(".zshrc", HOME)
+linker.link(".myshrc", HOME)
+linker.link("fish", CONFIG)
